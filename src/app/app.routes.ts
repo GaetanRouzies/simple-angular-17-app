b64b8303854332c61818bff0eda4dccdbe280d0a
Gaëtan Rouziès <rouzies.gaetan@gmail.com>
2024-02-02 10:38:08 +0100

    Reset project for video

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import {Routes} from '@angular/router';
-import {HomeComponent} from "./home/home.component";
 import {AnotherPageComponent} from "./another-page/another-page.component";
+import {HomeComponent} from "./home/home.component";
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  { path: 'another-page', component: AnotherPageComponent },
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'another-page', component: AnotherPageComponent},
 ];
