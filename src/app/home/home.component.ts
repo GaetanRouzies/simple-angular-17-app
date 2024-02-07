dde40a49638d305cc48c8a5012b8fb6c32e6f808
Gaëtan Rouziès <rouzies.gaetan@gmail.com>
2024-02-07 07:27:59 +0100

    Use Angular FontAwesome SVG package

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import {FaIconComponent} from "@fortawesome/angular-fontawesome";
+import {faAdd, faCoffee, faHome, faSatellite} from "@fortawesome/free-solid-svg-icons";
+import {faHackerNews} from "@fortawesome/free-brands-svg-icons";
+import {faAngry} from "@fortawesome/free-regular-svg-icons";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    FaIconComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
 
+  protected readonly faCoffee = faCoffee;
+  protected readonly faHome = faHome;
+  protected readonly faHackerNews = faHackerNews;
+  protected readonly faSatellite = faSatellite;
+  protected readonly faAdd = faAdd;
+  protected readonly faAngry = faAngry;
 }
